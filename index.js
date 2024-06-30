1d908cdcd036fc0b0cb8342aecc4922a78f885f1
henrym718 <henrym.718@hotmail.com>
2024-06-30 14:59:52 +0200

    add buildspec-test.yml file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 const app = express()
 const port = 4001
 
-
+    
 app.use(express.json())
 app.get("/", (req, res)=> res.json({status: 200}))
 app.listen(port, ()=> console.log("Server running on port" + port))
